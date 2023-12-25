717c97270cd735e53a848d12f31ee00b9ad2c957
Ruben Gomes <rbnpontes@gmail.com>
2023-12-25 11:10:33 -0300

    fix: build android script

--- build-android.bat
+++ build-android.bat
@@ -2,20 +2,20 @@
 set abi=armeabi-v7a
 
 echo Building Armeabi-v7a
-cmake -S . -B ./build-android -G "Ninja" -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%abi% -DANDROID_NATIVE_API_LEVEL=21
-cmake --build build-android --config Release
+cmake -S . -B ./build-android-%abi% -G "Ninja" -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%abi% -DANDROID_NATIVE_API_LEVEL=21
+cmake --build build-android-%abi% --config Release
 
 set abi=arm64-v8a
 echo Building Arm64-V8A
-cmake -S . -B ./build-android -G "Ninja" -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%abi% -DANDROID_NATIVE_API_LEVEL=21
-cmake --build build-android --config Release
+cmake -S . -B ./build-android-%abi% -G "Ninja" -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%abi% -DANDROID_NATIVE_API_LEVEL=21
+cmake --build build-android-%abi% --config Release
 
 set abi=x86
 echo Generating Project
-cmake -S . -B ./build-android -G "Ninja" -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%abi% -DANDROID_NATIVE_API_LEVEL=21
-cmake --build build-android --config Release
+cmake -S . -B ./build-android-%abi% -G "Ninja" -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%abi% -DANDROID_NATIVE_API_LEVEL=21
+cmake --build build-android-%abi% --config Release
 
 set abi=x86_64
 echo Generating Project
-cmake -S . -B ./build-android -G "Ninja" -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%abi% -DANDROID_NATIVE_API_LEVEL=21
-cmake --build build-android --config Release
+cmake -S . -B ./build-android-%abi% -G "Ninja" -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%abi% -DANDROID_NATIVE_API_LEVEL=21
+cmake --build build-android-%abi% --config Release
